6b1575631de631fe58985f377d63cf976a10ed0e
Connie <138333893+conniechen651@users.noreply.github.com>
2024-02-21 15:42:48 -0800

    Add option to toggle on and off rainbow colors

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
 const grid = document.querySelector("#grid");
 
 // default grid
-for(let i = 0; i < (16*16); i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
-    grid.appendChild(square);
-}
+createGrid(16);
 
 let squares = document.querySelectorAll(".square");
-
-function squareListener(item, size){
-    item.forEach((square) => {
-        square.addEventListener("mouseover", () => {
-            square.style.cssText = `background-color: rgb(100, 100, 100); width: ${size}px`;
-        });
-    });
-}
-
 squareListener(squares, 30);
 
-function createGrid(size){
-    let newSize = (496-size)/size;
-    // create new grid
-    for(let i = 0; i < (size*size); i++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        grid.appendChild(square);
-        square.style.cssText = `width: ${newSize}px`;
-    }
-}
-
-function clearGrid(){
-    squares.forEach((square) => {
-        grid.removeChild(square);
-    });
-}
-
-
 const adjustBtn = document.querySelector("#adjust-grid");
-
 adjustBtn.addEventListener("click", ()=> {
     // prompt for grid size
     let gridSize = 0;
@@ -60,9 +28,60 @@ adjustBtn.addEventListener("click", ()=> {
 });
 
 const clearBtn = document.querySelector("#clear");
-
 clearBtn.addEventListener("click", () => {
     squares.forEach((square) => {
         square.style.cssText ="background-color: white";
     });
 });
+
+let colorBtn = document.querySelector("#color");
+colorBtn.addEventListener("click", ()=> {
+    if (colorBtn.textContent === "Make Rainbow"){
+        squareListenerColor(squares, document.querySelector(".square").style.width);
+        colorBtn.textContent = "Make B&W";
+    } else {
+        squareListener(squares, document.querySelector(".square").style.width);
+        colorBtn.textContent = "Make Rainbow";
+    }
+    
+});
+
+// functions ---------------------
+function squareListener(item, size){
+    item.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.cssText = `background-color: rgb(100, 100, 100); width: ${size}px`;
+        });
+    });
+}
+
+function squareListenerColor(item, size){
+    item.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            let num1 = Math.floor(Math.random() * 256);
+            let num2 = Math.floor(Math.random() * 256);
+            let num3 = Math.floor(Math.random() * 256);
+            square.style.cssText = `background-color: rgb(${num1}, ${num2}, ${num3}); width: ${size}px`;
+        });
+    });
+}
+
+function createGrid(size){
+    let newSize = (496-size)/size;
+    // create new grid
+    for(let i = 0; i < (size*size); i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        grid.appendChild(square);
+        square.style.cssText = `width: ${newSize}px`;
+    }
+}
+
+function clearGrid(){
+    squares.forEach((square) => {
+        grid.removeChild(square);
+    });
+}
+
+
+
